f40afa51861349ee7b4adb758798efde539a47a0
RevaPooj <75774161+RevaPooj@users.noreply.github.com>
2021-02-12 09:34:45 +0530

    remember

--- distance.c
+++ distance.c
@@ -3,26 +3,34 @@
 #include <math.h>
 void input();
 void output(float d);
-void dist(float x1,float y1,float x2,float y2);
+void dis(float x1,float y1,float x2,float y2);
 
 void input()
 {
     float x1,y1,x2,y2;
-    printf("enter the values of x1 and y1:");
-    scanf("%f%f",&x1,&y1);
-    printf("enter the values of x2 and y2:");
-    scanf("%f%f",&x2,&y2);
-    dist(x1,y1,x2,y2);
+     
+     printf("enter the value of x1");
+     scanf("%f",&x1);
+         
+     printf("enter the value of y1");
+     scanf("%f",&y1);
+       
+     printf("enter the value of x2");
+     scanf("%f",&x2);
+       
+     printf("enter the value of y2");
+     scanf("%f",&y2);
+    dis(x1,y1,x2,y2);
 }
-void dist(float x1,float y1,float x2,float y2)
+void dis(float x1,float y1,float x2,float y2)
 {
-    float dist;
-    dist=((x2-x1)(x2-x1)+(y2-y1)(y2-y1));
-          output(dist);
+    float disti;
+      disti=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+         output(disti);
 }
 void output(float d)
 {
-    printf("the distance between 2 points is:%f",d);
+    printf("the distance between two points is:%f",d);
 }
 int main()
 {
